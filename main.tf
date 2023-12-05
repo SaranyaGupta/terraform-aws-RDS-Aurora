4053812dd4a1eed1c40c0ce5fea7474f41763b72
Saranya Gupta <saranya.gupta@coforge.com>
2023-12-05 15:17:36 +0530

    changed

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
   create_db_cluster_parameter_group=var.create_db_cluster_parameter_group
   create                           = var.create
   port                             = var.port
-  create_high_cpu_alarm            =var.create_high_cpu_alarm
+  create_high_cpu_alarm            = var.create_high_cpu_alarm
   create_storage_space_too_low_alarm= var.create_storage_space_too_low_alarm
   create_memory_too_low_alarm       = var.create_memory_too_low_alarm
 
