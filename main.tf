9e58069f9fedba0e7a17eede0395e670ba0b50e6
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-12 11:50:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
-locals {
-  create_db_subnet_group           = var.create_db_subnet_group
-  create_db_parameter_group        = var.create_db_parameter_group
-  create_cloudwatch_log_group      =var.create_cloudwatch_log_group
-  create_db_cluster_parameter_group=var.create_db_cluster_parameter_group
-  create                           = var.create
-  port                             = var.port
-  create_high_cpu_alarm            = var.create_high_cpu_alarm
-  create_storage_space_too_low_alarm= var.create_storage_space_too_low_alarm
-  create_memory_too_low_alarm       = var.create_memory_too_low_alarm
-
-}
-
 module "rds_cluster" {
   source = "./modules/aurora-mysql"
   depends_on = [module.db_subnet_group.db_subnet_group_name]
