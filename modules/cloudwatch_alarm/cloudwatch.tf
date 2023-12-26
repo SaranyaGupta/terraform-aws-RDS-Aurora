210ddf3526eb1401d31c6dbaab8a4b8537839ff2
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-26 18:42:19 +0530

    Update cloudwatch.tf

--- modules/cloudwatch_alarm/cloudwatch.tf
+++ modules/cloudwatch_alarm/cloudwatch.tf
@@ -31,7 +31,7 @@ resource "aws_cloudwatch_metric_alarm" "memory_too_low" {
   alarm_actions       = var.actions_alarm
   ok_actions          = var.actions_ok
   dimensions = {
-    DBInstanceIdentifier = var.db_instance_id[*]
+    DBInstanceIdentifier = var.db_instance_id
   }
 }
 
