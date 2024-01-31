ee5a0b1585d1d591f3850b8aa5777c56abe93725
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2024-01-31 14:38:49 +0530

    Update locals.tf

--- modules/aurora-mysql/locals.tf
+++ modules/aurora-mysql/locals.tf
@@ -6,5 +6,5 @@ locals {
   cluster_parameter_group_name  = var.db_cluster_parameter_group_name
   db_parameter_group_name       = var.db_parameter_group_name
   backtrack_window              = (var.engine == "aurora-mysql" || var.engine == "aurora")? var.backtrack_window : 0
-  create_monitoring_role        = local.create && var.create_monitoring_role && var.monitoring_interval > 0
+  create_monitoring_role        = local.create && var.create_monitoring_role
 }
